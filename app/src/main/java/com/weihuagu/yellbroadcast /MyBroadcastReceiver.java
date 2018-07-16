d8628e9030a58b9b6b5102c335414f7d15c66bff
weihuagu <weihuagu_work@163.com>
2018-07-16 23:41:30 +0800

    oo

---
+++ app/src/main/java/com/weihuagu/yellbroadcast /MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.weihuagu.yellbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by weihuagu.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Toast.makeText ( context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT ).show ();
+        abortBroadcast ();
+    }
+}
